ab0eb076fbcd93b92168236912a4b7b80d212197
Abd BASTOLA <b1162802@linux3.scedt.tees.ac.uk>
2022-02-10 12:51:42 +0000

    lab work

--- app/src/main/java/live/tees/B1162802/aBastola/labwork2/MainActivity.java
+++ app/src/main/java/live/tees/B1162802/aBastola/labwork2/MainActivity.java
@@ -5,30 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.Toast;
 
-import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button nextActivity;
-    private TextInputEditText fname,age,gender,email;
-    private EditText description;
+    private TextInputLayout fname,age,gender,email,description;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         nextActivity = (Button) findViewById(R.id.storeInformation);
-        fname = (TextInputEditText) findViewById(R.id.nameText);
-        age = (TextInputEditText) findViewById(R.id.ageText);
-        gender = (TextInputEditText) findViewById(R.id.genderText);
-        email = (TextInputEditText) findViewById(R.id.emailText);
-        description = (EditText) findViewById(R.id.description);
+        fname = (TextInputLayout) findViewById(R.id.nameText);
+        age = (TextInputLayout) findViewById(R.id.ageText);
+        gender = (TextInputLayout) findViewById(R.id.genderText);
+        email = (TextInputLayout) findViewById(R.id.emailText);
+        description = (TextInputLayout) findViewById(R.id.description);
         nextActivity.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        
+        if(fname.getEditText().getText().toString().isEmpty()){
+            fname.setError("Hello");
+            fname.requestFocus();
+        }else{
+            fname.setError(null);
+        }
+
+        Toast.makeText(this,"this is is",Toast.LENGTH_LONG).show();
+
+
+
     }
 }
