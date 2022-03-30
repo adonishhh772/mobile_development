87e3ec4c10588932ed4867f226e914defb57ab1e
Abd Bastola <abdbastola@Abds-MacBook-Air.local>
2022-03-30 13:56:32 +0100

    Project Initiated

---
+++ app/src/main/java/uk/tees/b1162802/boro/MainActivity.java
@@ -0,0 +1,14 @@
+package uk.tees.b1162802.boro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
